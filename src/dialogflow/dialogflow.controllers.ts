059b08ef591c3742cfa2aca0f9f6c2846d959cf1
Ahmad Hayam <ahmad.hayam@gdplabs.id>
2019-11-13 07:19:10 +0700

    Implemented simulasi kredit

--- src/dialogflow/dialogflow.controllers.ts
+++ src/dialogflow/dialogflow.controllers.ts
@@ -1,3 +1,4 @@
+import { SIMULASI_KREDIT, simulasiKreditFulfillment } from './fulfillments/4.simulasikredit';
 import { tersangkaFulfillment, TERSANGKA } from './fulfillments/2.followup';
 import { daftarSIMFulfillment, DAFTAR_SIM } from './fulfillments/1.simple';
 import { Controller, Post, Get, Body } from '@nestjs/common';
@@ -13,6 +14,7 @@ export class DialogflowController {
         this.fulfillments[DAFTAR_SIM] = daftarSIMFulfillment;
         this.fulfillments[TERSANGKA] = tersangkaFulfillment;
         this.fulfillments[TEBAK_TEBAKAN] = tebakAngkaFulfillment;
+        this.fulfillments[SIMULASI_KREDIT] = simulasiKreditFulfillment;
     }
 
     @Get('webhook')
---
+++ src/dialogflow/fulfillments/4.simulasikredit.ts
@@ -0,0 +1,42 @@
+import { WebhookRequest, WebhookResponse } from 'dialogflow';
+
+import { validateAndResetIfWrong, Field } from './utils';
+
+export const SIMULASI_KREDIT = 'Simulasi Kredit - Filling';
+
+export const simulasiKreditFulfillment = (body: WebhookRequest): WebhookResponse | undefined => {
+    const eventName = 'simulasi-kredit-filling-evt';
+    const response: WebhookResponse = {};
+    const eventCtx = ((body.queryResult.outputContexts || []).filter(x => x.name.endsWith(eventName)) || [undefined])[0];
+
+    const fields: Array<Field<any>> = [
+        {
+            name: 'nominalKredit',
+            extractor: ((parameter) => parseFloat(parameter.nominalKredit)),
+            validator: ((val: number) => (val > 10_000_000 && val < 1_000_000_000)),
+        },
+        {
+            name: 'nominalDP',
+            extractor: ((parameter) => parseFloat(parameter.nominalDP)),
+            validator: ((val: number, param: any) => {
+                const loan = parseFloat(param.nominalKredit);
+                const dp = val;
+
+                return dp / loan > 0.1 && dp / loan < 0.3;
+            }),
+        },
+    ];
+
+    const validationResult = validateAndResetIfWrong(body, fields, eventName);
+    if (validationResult) {
+        return validationResult;
+    }
+
+    if (eventCtx && eventCtx.parameters && eventCtx.parameters.wrongAnswer) {
+        response.fulfillmentText = 'Jawaban anda salah\n' + body.queryResult.fulfillmentText;
+        eventCtx.parameters.wrongAnswer = false;
+        response.outputContexts = [eventCtx];
+    }
+
+    return response;
+};
